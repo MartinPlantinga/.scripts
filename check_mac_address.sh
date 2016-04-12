54b2182e9fd9de3fb9fb8234e9d407e26b4407fe
Martin <m.plantinga@student.tue.nl>
2016-04-12 22:21:54 +0200

    added comments

--- check_mac_address.sh
+++ check_mac_address.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-MAC_ETH=$(ifconfig | grep eth | grep -i hwaddr)
-if [ -n "$VAR" ]; then
-	MAC_ETH=${MAC_ETH##*HWaddr}
+MAC_ETH=$(ifconfig | grep eth | grep -i hwaddr)		# extract line that contains "eth" and "hwaddr" from ifconfig
+if [ -n "$VAR" ]; then								# if variable not empty
+	MAC_ETH=${MAC_ETH##*HWaddr}						# extract mac address to variable
 fi
 
-MAC_WLAN=$(ifconfig | grep wlan | grep -i hwaddr)
-if [ -n "$VAR" ]; then
-	MAC_WLAN=${MAC_WLAN##*HWaddr}
+MAC_WLAN=$(ifconfig | grep wlan | grep -i hwaddr)	# extract line that contains "eth" and "hwaddr" from ifconfig
+if [ -n "$VAR" ]; then								# if variable not empty
+	MAC_WLAN=${MAC_WLAN##*HWaddr}					# extract mac address to variable
 fi
