5fe8283755e071d1a828b515c8c1638c4f9418da
Martin <m.plantinga@student.tue.nl>
2016-04-12 22:37:07 +0200

    added functionality to check old mac address

--- check_mac_address.sh
+++ check_mac_address.sh
@@ -9,3 +9,17 @@ MAC_WLAN=$(ifconfig | grep wlan | grep -i hwaddr)	# extract line that contains "
 if [ -n "$VAR" ]; then								# if variable not empty
 	MAC_WLAN=${MAC_WLAN##*HWaddr}					# extract mac address to variable
 fi
+
+
+filepath="/home/ebox"								# determine path
+
+if [ ! -f $filepath/.mac_address]; then				# check if file exists 
+	touch $filepath/.mac_address					# if non-existing add file
+else
+	OLD_MAC_WLAN=$(<$filepath/.mac_address)			# read old mac address into file
+
+	if [ $MAC_WLAN != OLD_MAC_WLAN]; then
+		rm /home/ebox/MATLAB/R2015b/licenses/*.lic 	# change this
+	fi
+fi
+
