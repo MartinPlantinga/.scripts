66d18a271fe9cec90e2760153e7ff867c8a546b8
Martin <m.plantinga@student.tue.nl>
2016-04-13 07:56:44 +0200

    changed file path to /home/ebox/.scripts and added check_eth_port.sh. Latter has to be verified

--- check_mac_address.sh
+++ check_mac_address.sh
@@ -5,6 +5,9 @@
 # 1. the Matlab license is removed
 # 2. WIFI and VPN connections are removed
 
+filepath="/home/ebox/.scripts/mac_verification"
+
+
 MAC_ETH=$(ifconfig | grep eth | grep -i hwaddr)		# extract line that contains "eth" and "hwaddr" from ifconfig
 if [ -n "$VAR" ]; then								# if variable not empty
 	MAC_ETH=${MAC_ETH##*HWaddr}						# extract mac address to variable
@@ -15,13 +18,10 @@ if [ -n "$VAR" ]; then								# if variable not empty
 	MAC_WLAN=${MAC_WLAN##*HWaddr}					# extract mac address to variable
 fi
 
-
-filepath="/home/ebox"								# determine path
-
-if [ ! -f $filepath/.mac_address]; then				# check if file exists 
-	touch $filepath/.mac_address					# if non-existing add file
+if [ ! -f $filepath/mac_address]; then				# check if file exists 
+	touch $filepath/mac_address						# if non-existing add file
 else
-	OLD_MAC_WLAN=$(<$filepath/.mac_address)			# read old mac address into variable
+	OLD_MAC_WLAN=$(<$filepath/mac_address)			# read old mac address into variable
 
 	if [ $MAC_WLAN != OLD_MAC_WLAN]; then
 		rm /home/ebox/MATLAB/R2015b/licenses/*.lic 	# rm license, possible path1
