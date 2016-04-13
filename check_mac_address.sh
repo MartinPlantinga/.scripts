dc3112b0e7361554e1c4357c589416d4a35dc42a
Martin <m.plantinga@student.tue.nl>
2016-04-13 07:33:51 +0200

    ready for testing

--- check_mac_address.sh
+++ check_mac_address.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script checks if the Edrive has been inserted into another computer by checking if the MAC address of the wifi card has changed
+# If the Edrive has been inserted into another computer:
+# 1. the Matlab license is removed
+# 2. WIFI and VPN connections are removed
+
 MAC_ETH=$(ifconfig | grep eth | grep -i hwaddr)		# extract line that contains "eth" and "hwaddr" from ifconfig
 if [ -n "$VAR" ]; then								# if variable not empty
 	MAC_ETH=${MAC_ETH##*HWaddr}						# extract mac address to variable
@@ -16,10 +21,13 @@ filepath="/home/ebox"								# determine path
 if [ ! -f $filepath/.mac_address]; then				# check if file exists 
 	touch $filepath/.mac_address					# if non-existing add file
 else
-	OLD_MAC_WLAN=$(<$filepath/.mac_address)			# read old mac address into file
+	OLD_MAC_WLAN=$(<$filepath/.mac_address)			# read old mac address into variable
 
 	if [ $MAC_WLAN != OLD_MAC_WLAN]; then
-		rm /home/ebox/MATLAB/R2015b/licenses/*.lic 	# change this
+		rm /home/ebox/MATLAB/R2015b/licenses/*.lic 	# rm license, possible path1
+		rm /root/.matlab/R2015b_licenses/*.lic 		# rm license, possible path2
+		rm /etc/NetworkManager/system-connections/*	# rm all wifi and VPN connections
 	fi
 fi
 
+exit 0
